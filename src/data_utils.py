9d4f66eb505c4942a02bfd1481d2f882e1285647
Descamps <adescamp@SIMES-PC93495.local>
2017-05-28 13:34:44 -0700

    Modify data_utils to add output

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,48 +1,51 @@
 import argparse
 import json
-
+def extract_data(data_path):
 # Instantiate the parser
-parser = argparse.ArgumentParser(description='Get data cleaning arguments')
+	parser = argparse.ArgumentParser(description='Get data cleaning arguments')
 
-parser.add_argument('data_path', type=str,
-                    help='Relative source data path, including file name')
+	parser.add_argument('data_path', type=str,
+	                    help='Relative source data path, including file name')
 
-parser.add_argument('out_path', type=str,
-                    help='Relative data output path, including file name')
+	# parser.add_argument('out_path', type=str,
+	#                     help='Relative data output path, including file name')
 
-#Parse
-args = parser.parse_args()
-
-
-#Clean data
-
-data_path = args.data_path
-out_path = args.out_path
-
-print(data_path)
-print(out_path)
+	#Parse
+	args = parser.parse_args()
 
 
-if "." in data_path:
-	if data_path.split(".")[1] != "json":
-		raise ValueError("Invalid Extension")
-else:
-	data_path + ".json"
+	#Clean data
+
+	data_path = args.data_path
+	out_path = args.out_path
+
+	print(data_path)
+	print(out_path)
 
 
-if "." in out_path:
-	if out_path.split(".")[1] != "csv":
-		raise ValueError("Invalid Extension")
-else:
-	out_path + ".csv"
+	if "." in data_path:
+		print("gcjgfx", data_path.split(".")[-1])
+		if data_path.split(".")[-1] != "json":
+			raise ValueError("Invalid Extension")
+	else:
+		data_path + ".json"
 
 
-data = []
+	# if "." in out_path:
+	# 	print("qwerty", out_path.split(".")[-1])
+	# 	if out_path.split(".")[-1] != "csv" and out_path.split(".")[-1] != "txt":
+	# 		raise ValueError("Invalid Extension")
+	# else:
+	# 	out_path + ".csv"
 
-for line in open(data_path, 'r'):
-	data.append(json.loads(line))	
 
-print data[1000]
+	data = []
+
+	for line in open(data_path, 'r'):
+		data.append(json.loads(line))	
+
+	print len(data)
+	return data
 
 
 
